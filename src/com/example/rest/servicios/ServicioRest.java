4ccca4af9be27a96e5d33204f783f1bc3e40e319
suyinsilvacontreras <mishelo2000@gmail.com>
2020-06-20 17:43:58 -0500

    Se agrego el segundo commit

--- src/com/example/rest/servicios/ServicioRest.java
+++ src/com/example/rest/servicios/ServicioRest.java
@@ -1,7 +1,10 @@
 package com.example.rest.servicios;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,7 +13,9 @@ import javax.ws.rs.core.Response;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.example.rest.dao.MarcaModel;
 import com.example.rest.dao.UsuarioModel;
+import com.example.rest.entidades.Marca;
 import com.example.rest.entidades.Usuario;
 
 @Path("/servicios")
@@ -19,7 +24,9 @@ import com.example.rest.entidades.Usuario;
 public class ServicioRest {
 	private static final Log log = LogFactory.getLog(ServicioRest.class);
 	private UsuarioModel daoUser = new UsuarioModel();
+	private MarcaModel daoMarca = new MarcaModel();
 
+	
 	@GET
 	@Path("/login")
 	public Response login(Usuario obj) {
@@ -34,6 +41,46 @@ public class ServicioRest {
 		return Response.ok(daoUser.listarTodos()).build();
 	}
 
+	@GET
+	@Path("/marca")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response listarMarcaTodos() {
+		log.info("listarTodos rest ");
+		return Response.ok(daoUser.listarTodos()).build();
+	}
+
+	@POST
+	@Path("/marca")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response registraMarca(Marca obj) {
+		log.info("Registra usuario " + obj.getIdMarca());
+		if (daoMarca.insertaMarca(obj) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+
+	@PUT
+	@Path("/marca")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response atualizaMarca(Marca obj) {
+		log.info("Actualiza usuario " + obj.getIdMarca());
+		if (daoMarca.actualizaMarca(obj) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
+
+	@DELETE
+	@Path("/marca/{idMarca}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response eliminaMarca(int id) {
+		log.info("Elimina usuario " + id);
+		if (daoMarca.eliminaMarca(id) > 0)
+			return Response.ok().build();
+		else
+			return Response.notModified().build();
+	}
 	
 
 }
